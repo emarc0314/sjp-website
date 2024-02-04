5aa95f3ad31a8e8b7f860e8c32d32e953e04a9f4
em662 <em662@cornell.edu>
2024-02-03 19:06:21 -0500

    elvis changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          if you see this, it works -elvis
         </p>
         <a
           className="App-link"
